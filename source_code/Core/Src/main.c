3092c27666cc6bfcd6ccc8468c9da178dbfe558f
DESKTOP-5K2RFED\Admin <todiuquang123@gmail.com>
2023-09-28 09:09:48 +0700

    update

--- source_code/Core/Src/main.c
+++ source_code/Core/Src/main.c
@@ -221,14 +221,14 @@ int led_status =1;
 		  case 1:
 			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
 			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-			  led_status =2;
 			  display7SEG(1);
+			  led_status =2;
 			  break;
 		  case 2:
 			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
 			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
-			  display7SEG(2);
 			  led_status=1;
+			  display7SEG(2);
 			  break;
 		  default:
 			  break;
